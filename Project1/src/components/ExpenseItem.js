1d11a67e74f9bb83ed3c3d4c62767164dd9946e1
Scott Minter <sminter@gmail.com>
2023-09-14 11:14:18 -0400

    ongoing course work using this project

---
+++ Project1/src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem({ date, title, amount }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
